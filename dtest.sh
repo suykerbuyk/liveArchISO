9e4fbe11b53af36eb0529abafee2be351ac77536
John Suykerbuyk <git@suykerbuyk.org>
2022-12-03 21:06:59 -0700

    Mostly works end to end.
    * Still needs user adds.
    * Cleaned up logging
    * Added ability to wait for partitions to show up in the format
      function.

--- dtest.sh
+++ dtest.sh
@@ -16,7 +16,8 @@ SYS_NAME="arch"
 DIR_MNT="/mnt"
 DIR_BOOT="${DIR_MNT}/boot"
 DIR_ESP="${DIR_BOOT}/esp"
-TGT_TIME_ZONE="../usr/share/zoneinfo/America/Denver"
+#TGT_TIME_ZONE="../usr/share/zoneinfo/America/Denver"
+TGT_TIME_ZONE="/usr/share/zoneinfo/America/Denver"
 archzfs_pgp_key="F75D9D76"
 
 # Set a default locale during install to avoid mandb error when indexing man pages
@@ -25,11 +26,10 @@ export LANG=C
 # This is required to fix grub's "failed to get canonical path" error
 export ZPOOL_VDEV_NAME_PATH=1
 declare -a DRV_LIST
+declare -a EFI_PARTS
 declare -a BOOT_PARTS
 declare -a SWAP_PARTS
 declare -a ROOT_PARTS
-declare -a BIOS_PARTS
-declare -a EFI_PARTS
 
 # Colors
 RED='\033[0;31m'
@@ -108,7 +108,7 @@ run() {
 # Run stuff in the ZFS chroot install function with optional message
 chrun() {
     [[ -n "${2}" ]] && msg "arch-chroot ${2}"
-    arch-chroot "${installdir}" /bin/bash -c "${1}"
+    arch-chroot "${DIR_MNT}" /bin/bash -c "${1}"
 }
 
 capture_stderr () {
@@ -167,12 +167,13 @@ get_disk_list() {
 	[ -f "${MENU_FILE}" ] && rm "${MENU_FILE}"
 }
 prepare_for_start() {
+	msg "\nRUN: ${FUNCNAME[0]}\n"
 	for MNT in $(cat /proc/mounts | grep ${DIR_MNT} | awk '{print $2}' | sort -r) ; do 
 		run "umount ${MNT}" || true
 	done
 	zpool destroy ${ZFS_BOOT_POOL} || true
 	zpool destroy ${ZFS_ROOT_POOL} || true
-	msg "Zapping selected disk"
+	msg "** Zapping selected disk"
 	# vgchange -an &> /dev/null
 	# mdadm --zero-superblock --force "${1}" &> /dev/null
 	for DRV in  ${DRV_LIST[*]}
@@ -186,18 +187,17 @@ prepare_for_start() {
 		wait
 	done
 	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done
-	msg "Partitioning"
+	msg "** Partitioning"
 	for DRV in ${DRV_LIST[*]}
 	do
 		run "sgdisk -n1:1M:+1G    -t1:EF00  -c1:efi  \
-		        -a1 -n5:24k:+100K -t5:EF02  -c5:bios \
 		            -n2:0:+4G     -t2:BE00  -c2:boot \
 		            -n3:0:-8G     -t3:BF00  -c3:root \
 		            -n4:0:0       -t4:8308  -c4:swap ${DRV}" &
 	done
 	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done
 	wait
-	msg "Partprobing disk"
+	msg "** Partprobing disk"
 	for DRV in ${DRV_LIST[*]} 
 	do
 		run "partprobe -s ${DRV} && udevadm trigger ${DRV}" & 
@@ -205,40 +205,54 @@ prepare_for_start() {
 		BOOT_PARTS+=("${DRV}-part2")
 		ROOT_PARTS+=("${DRV}-part3")
 		SWAP_PARTS+=("${DRV}-part4")
-		BIOS_PARTS+=("${DRV}-part5")
        	done
-	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done
+	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done;
 	wait
-	udevadm trigger
+	run "udevadm trigger"
 }
 create_file_systems() {
-	msg "Formatting boot partitions"
-	for PART in ${BOOT_PARTS[*]}; do run "mkfs.vfat $PART"  & done
-	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done
-	msg "Formatting swap partitions"
-	for PART in ${SWAP_PARTS[*]}; do run "mkswap $PART"     & done
-	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done
+	msg "\nRUN: ${FUNCNAME[0]}\n"
+	partprobe -s
+	udevadm trigger
+	msg "** Formatting boot partitions"
+	for PART in ${EFI_PARTS[*]} ${SWAP_PARTS[*]} ${ROOT_PARTS[*]} ${BOOT_PARTS[*]} 
+	do 
+		X=10
+		while [ $X ] 
+		do
+			echo "waiting for $PART" 
+			[[ -e $PART ]] && break || sleep .2 && X=$((X-1))
+		done
+	done
+	for PART in ${EFI_PARTS[*]}; do run "mkfs.vfat $PART"; done
+	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done; wait
+	msg "** Formatting swap partitions"
+	for PART in ${SWAP_PARTS[*]}; do run "mkswap $PART" ; done
+	for job in `jobs -p`; do echo "* Waiting for job: $job to complete"; wait ${job}; done; wait
 	ZFS_VDEVS=""
 	for PART in ${ROOT_PARTS[*]}; do ZFS_ROOT_VDEVS+="$PART "; done
 	for PART in ${BOOT_PARTS[*]}; do ZFS_BOOT_VDEVS+="$PART "; done
 	
-	msg "Creating ${ZFS_ROOT_POOL} pool"
-
+	msg "** Creating ${ZFS_ROOT_POOL} pool"
 	run "zpool create -f ${ZFS_BOOT_ATTRS} -R ${DIR_MNT} ${ZFS_BOOT_POOL} raidz2 $ZFS_BOOT_VDEVS"
 	run "zpool create -f ${ZFS_ROOT_ATTRS} -R ${DIR_MNT} ${ZFS_ROOT_POOL} raidz2 $ZFS_ROOT_VDEVS"
 	# Create container datasets
+	msg "** Creating container data sets"
 	run "zfs create -o canmount=off -o mountpoint=none ${ZFS_BOOT_POOL}/BOOT"
 	run "zfs create -o canmount=off -o mountpoint=none ${ZFS_ROOT_POOL}/ROOT"
 	run "zfs create -o canmount=off -o mountpoint=none ${ZFS_ROOT_POOL}/DATA"
 	# Create root and boot file systems
+	msg "** Creating root zfs file systems for 'root' and 'boot'"
 	run "zfs create -o mountpoint=legacy -o canmount=noauto ${ZFS_BOOT_POOL}/BOOT/default"
 	run "zfs create -o mountpoint=/      -o canmount=noauto ${ZFS_ROOT_POOL}/ROOT/default"
+	msg "** mounting ${ZFS_ROOT_POOL} file system"
 	run "zfs mount ${ZFS_ROOT_POOL}/ROOT/default"
+	msg "** mounting ${ZFS_BOOT_POOL} file system"
 	run "mkdir ${DIR_MNT}/boot"
 	run "mount -t zfs ${ZFS_BOOT_POOL}/BOOT/default ${DIR_MNT}/boot"
 
 	# Creates datasets within the root file system
-	msg "Creating system data sets"
+	msg "** Creating system data sets"
 	run "zfs create -o mountpoint=/ -o canmount=off ${ZFS_ROOT_POOL}/DATA/default"
 	for i in {usr,var,var/lib}
 	do
@@ -255,14 +269,15 @@ create_file_systems() {
 	done
 	run "chmod 750  ${DIR_MNT}/root"
 	run "chmod 1777 ${DIR_MNT}/var/tmp"
-	msg "Creating first efi system partition"
+	msg "** Creating first efi system partition"
 	# Format and mount EFI system partition
 	run "mkfs.vfat -n EFI ${EFI_PARTS[0]}"
-	run "mkdir ${DIR_MNT}/boot/efi"
+	run "mkdir ${DIR_MNT}/boot/esp"
 	# We'll tend to the other efi parts later...	
-	run "mount -t vfat ${EFI_PARTS[0]} ${DIR_MNT}/boot/efi"
+	run "mount -t vfat ${EFI_PARTS[0]} ${DIR_ESP}"
 }
 generate_mounts() {
+	msg "\nRUN: ${FUNCNAME[0]}\n"
 # tab-separated zfs properties
 # see /etc/zfs/zed.d/history_event-zfs-list-cacher.sh
 export \
@@ -273,22 +288,24 @@ PROPS="name,mountpoint,canmount,atime,relatime,devices,exec\
 ,org.openzfs.systemd:wanted-by,org.openzfs.systemd:required-by\
 ,org.openzfs.systemd:nofail,org.openzfs.systemd:ignore"
 
-	mkdir -p ${DIR_MNT}/etc/zfs/zfs-list.cache
+	run mkdir -p ${DIR_MNT}/etc/zfs/zfs-list.cache
 
 	zfs list -H -t filesystem -o $PROPS -r ${ZFS_ROOT_POOL} \
 	> ${DIR_MNT}/etc/zfs/zfs-list.cache/${ZFS_ROOT_POOL}
 
-	sed -Ei "s|${DIR_MNT}/?|/|" ${DIR_MNT}/etc/zfs/zfs-list.cache/*
+	run "sed -Ei 's|${DIR_MNT}/?|/|' ${DIR_MNT}/etc/zfs/zfs-list.cache/*"
 
 	echo ${ZFS_BOOT_POOL}/BOOT/default /boot zfs rw,xattr,posixacl 0 0 >>/${DIR_MNT}/etc/fstab
-	echo UUID=$(blkid -s UUID -o value ${EFI_PARTS[0]}) /boot/efi vfat umask=0022,fmask=0022,dmask=0022 0 1 >> ${DIR_MNT}/etc/fstab
+	echo UUID=$(blkid -s UUID -o value ${EFI_PARTS[0]}) /boot/esp vfat umask=0022,fmask=0022,dmask=0022 0 1 >> ${DIR_MNT}/etc/fstab
 }
 do_install() {
-	pacstrap -C ./local.pacman.conf ${DIR_MNT} $(cat ./packages.x86_64 | grep -v '#')
+	echo "Running: do_install"
+	run pacstrap -C ./local.pacman.conf ${DIR_MNT} $(cat ./packages.x86_64 | grep -v '#')
 }
 
 do_configure() {
-	msg "Configuring network"
+	msg "\nRUN: ${FUNCNAME[0]}\n"
+	msg "  Configuring network"
 	cat <<- END_OF_NET_CONF > ${DIR_MNT}/etc/systemd/network/20-ethernet.netwok
 	#
 	# SPDX-License-Identifier: GPL-3.0-or-later
@@ -304,17 +321,17 @@ do_configure() {
 	[DHCP]
 	RouteMetric=512
 	END_OF_NET_CONF
-	msg "Setting hostname to $TGT_HOSTNAME"
+	msg "** Setting hostname to $TGT_HOSTNAME"
 	echo $TGT_HOSTNAME>/${DIR_MNT}/etc/hostname 
-	msg "Setting time zone to $TGT_TIME_ZONE"
-	ln -s $TGT_TIME_ZONE ${DIR_MNT}/etc/localtime
-	hwclock --systohc
-	msg "Configuring /etc/hosts"
+	msg "** Setting time zone to $TGT_TIME_ZONE"
+	run ln -s $TGT_TIME_ZONE ${DIR_MNT}/etc/localtime
+	run hwclock --systohc
+	msg "** Configuring /etc/hosts"
 	echo "127.0.0.1		localhost">>/${DIR_MNT}/etc/hostname 
 	echo "::1		localhost">>/${DIR_MNT}/etc/hostname 
 	echo "127.0.1.1		$TGT_HOSTNAME">>/${DIR_MNT}/etc/hostname 
 
-	msg "Configuring pacman"
+	msg "** Configuring pacman"
 	tee -a ${DIR_MNT}/etc/pacman.conf <<- 'PACMAN_CONF'
 	[archzfs]
 	Include = /etc/pacman.d/mirrorlist-archzfs
@@ -327,44 +344,65 @@ do_configure() {
 	Server = https://mirror.in.themindsmaze.com/archzfs/$repo/$arch
 	PACMAN_MIRRORS
 
-	msg "Setting language to: $TGT_LANGUAGE"
-	sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' ${DIR_MNT}/etc/locale.gen
+	msg "** Setting language to: $TGT_LANGUAGE"
+	run "sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' ${DIR_MNT}/etc/locale.gen"
 	echo "LANG=en_US.UTF-8" >> ${DIR_MNT}/etc/locale.conf
-	arch-chroot /mnt locale-gen
+	#arch-chroot /mnt locale-gen
+	chrun "locale-gen"
 
-	msg "Configuring mkinitcpio"
-	mv ${DIR_MNT}/etc/mkinitcpio.conf ${DIR_MNT}/etc/mkinitcpio.conf.original
+	msg "** Configuring mkinitcpio"
+	run mv ${DIR_MNT}/etc/mkinitcpio.conf ${DIR_MNT}/etc/mkinitcpio.conf.original
 	tee ${DIR_MNT}/etc/mkinitcpio.conf <<-MKINIT_EOF
 	HOOKS=(base udev autodetect modconf block keyboard zfs filesystems)
 	MKINIT_EOF
+	msg "** Configuring systemd update hook"
+	run "mkdir -p ${DIR_MNT}/etc/pacman.d/hooks"
+	tee ${DIR_MNT}/etc/pacman.d/hooks/100-systemd-boot.hook <<-MKSYSD_HOOK
+	[Trigger]
+	Type = Package
+	Operation = Upgrade
+	Target = systemd
+
+	[Action]
+	Description = update systemd-boot
+	When = PostTransaction
+	Exec = /usr/bin/bootctl update
+	MKSYSD_HOOK
 	# Enable root ssh
-	sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' ${DIR_MNT}/etc/ssh/sshd_config
+	msg "** enabling root ssh"
+	run "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' ${DIR_MNT}/etc/ssh/sshd_config"
 	#zgenhostid -f -o ${DIR_MNT}/etc/hostid
-	systemd-machine-id-setup --root=${DIR_MNT}
-	arch-chroot ${DIR_MNT} pacman-key -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
-	arch-chroot ${DIR_MNT} pacman-key  --lsign DDF7DB817396A49B2A2723F7403BD972F75D9D76
-	#arch-chroot ${DIR_MNT} bootctl install
-	bootctl --path=${DIR_MNT}/boot/efi install
-	arch-chroot ${DIR_MNT} mkdir -p "$DIR_ESPECTORY/loader/entries/"
-	arch-chroot ${DIR_MNT} zpool set cachefile=/etc/zfs/zpool.cache ${ZFS_ROOT_POOL}
-	arch-chroot ${DIR_MNT} zpool set cachefile=/etc/zfs/zpool.cache ${ZFS_BOOT_POOL}
-	systemctl enable  --root=${DIR_MNT} \
+	run systemd-machine-id-setup --root=${DIR_MNT}
+	chrun "pacman-key -r DDF7DB817396A49B2A2723F7403BD972F75D9D76"
+	chrun "pacman-key  --lsign DDF7DB817396A49B2A2723F7403BD972F75D9D76"
+	run "bootctl --path=${DIR_ESP} install"
+	run "mkdir -p $DIR_ESP/loader/entries/"
+	chrun "zpool set cachefile=/etc/zfs/zpool.cache ${ZFS_ROOT_POOL}"
+	chrun "zpool set cachefile=/etc/zfs/zpool.cache ${ZFS_BOOT_POOL}"
+	run "systemctl enable  --root=${DIR_MNT} \
 		cockpit.service \
 		sshd.service \
 		systemd-timesyncd \
 		zfs-import-cache.service \
 		zfs-import.target \
 		zfs-mount.service \
-		zfs.target
-	arch-chroot ${DIR_MNT} mkinitcpio -P
+		zfs.target"
+	chrun "mkinitcpio -P"
 	echo "Working on: ${DIR_ESP}/loader/loader.conf"
-	cat <<EOT > "${DIR_ESP}/loader/loader.conf"
-# alis
-timeout 5
-default archlinux.conf
-editor 0
-EOT
-
+	tee "${DIR_ESP}/loader/loader.conf" <<-LOADER_CONF
+	timeout 5
+	default archlinux.conf
+	editor 0
+	LOADER_CONF
+	msg "** Configuring arch boot config"
+	tee ${DIR_MNT}/boot/esp/loader/entries/archlinux.conf <<-ARCHLINUX_CONF
+	title           Arch Linux
+	linux           vmlinuz-linux
+	initrd          amd-ucode.img
+	initrd          intel-ucode.img
+	initrd          initramfs-linux.img
+	options         zfs=$ZFS_ROOT_POOL/ROOT/default rw
+	ARCHLINUX_CONF
 #	umount -R ${DIR_MNT}
 #	zpool export -a
 }
